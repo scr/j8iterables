ca0da3a5a5a0f95e5fdf1731c2c6b58e9c0a76d4
Sheridan C Rawlins <scr@yahoo-inc.com>
2015-10-20 16:20:24 -0700

    Need sudo afterall - gnupg2 is not allowed as a package dependency.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,6 +33,11 @@ function install_gpg_key() {
     gpg2 --batch --import scr.travis.gpg.asc
 }
 
+function install_gpg() {
+    sudo apt-get update -qq
+    sudo apt-get install -y gnupg2
+}
+
 function mvn_deploy() {
     local -r SETTINGS_FILE="$1"
     mvn clean deploy -Drelease -s "$SETTINGS_FILE"
@@ -48,6 +53,7 @@ function main() {
     local -r ENC_GPG_KEY_FILE="$2"
     local -r MVN_SETTINGS_FILE="$3"
 
+    install_gpg
     install_gpg_key $(encryption_var_name "$HASH" key) $(encryption_var_name "$HASH" iv) "$ENC_GPG_KEY_FILE" "${ENC_GPG_KEY_FILE/.asc}"
     mvn_deploy "$MVN_SETTINGS_FILE"
 }
