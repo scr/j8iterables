7e162e76255a30b49fb077ed97484e785a3bfdc3
Sheridan C Rawlins <sheridan.rawlins@yahoo.com>
2015-10-20 14:13:33 -0700

    Update installGPG.sh
    Import key in batch mode

--- scripts/installGPG.sh
+++ scripts/installGPG.sh
@@ -3,4 +3,4 @@ source "$(dirname $0)/common.sh" || exit -1
 checkTravisSecure
 
 openssl aes-256-cbc -K $encrypted_ae4d2db0dbde_key -iv $encrypted_ae4d2db0dbde_iv -in scr.travis.gpg.asc.enc -out scr.travis.gpg.asc -d
-gpg2 --import scr.travis.gpg.asc
+gpg2 --batch --import scr.travis.gpg.asc
