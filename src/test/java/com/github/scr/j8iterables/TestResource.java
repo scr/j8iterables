42aa29e610a06ee44852572f12c95f038c9e3243
Sheridan C Rawlins <scr@yahoo-inc.com>
2017-02-24 15:06:38 -0800

    Initialize TestResource with collection.

--- src/test/java/com/github/scr/j8iterables/TestResource.java
+++ src/test/java/com/github/scr/j8iterables/TestResource.java
@@ -1,16 +1,14 @@
 package com.github.scr.j8iterables;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by scr on 2/24/17.
  */
 public class TestResource extends ArrayList<Integer> implements AutoCloseable {
     public TestResource() {
-        super(3);
-        add(1);
-        add(2);
-        add(3);
+        super(Arrays.asList(1, 2, 3));
     }
 
     @Override
